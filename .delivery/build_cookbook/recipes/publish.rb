e1b165f68c8fefdb535adf9bd26af33ce2219a65
azure user <azure@me.me>
2017-11-20 20:01:40 +0000

    updated publish

--- .delivery/build_cookbook/recipes/publish.rb
+++ .delivery/build_cookbook/recipes/publish.rb
@@ -6,16 +6,16 @@
 include_recipe 'delivery-truck::publish'
 
 changed_cookbooks.each do |cookbook|
-  directory '/home/azure/cookbooks' do
-    owner 'root'
-    group 'root'
+  directory '/home/job_runner/cookbooks' do
+    owner 'job_runner'
+    group 'job_runner'
     mode '0755'
     action :create
   end
 
   execute "download_cookbook_#{cookbook.name}" do
     command "knife cookbook download #{cookbook.name} #{cookbook.version}"
-    cwd '/home/azure/cookbooks'
+    cwd '/home/job_runner/cookbooks'
     action :run
   end
 end
