406a0c4b15f352fd54c1743e765ba83174e5d570
azure user <azure@me.me>
2017-11-20 21:30:11 +0000

    new depends

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures test2'
 long_description 'Installs/Configures test2'
-version '0.1.1'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+depends 'chef-vault'
+depends 'test3'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
