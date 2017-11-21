cb53bfe0d19bbec2ca36ade29b6b0f2850b35321
azure user <azure@me.me>
2017-11-21 19:22:17 +0000

    test accept

--- .delivery/build_cookbook/recipes/smoke.rb
+++ .delivery/build_cookbook/recipes/smoke.rb
@@ -4,3 +4,15 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::smoke'
+
+search_query = "recipes:#{node['delivery']['change']['project']}* AND " \
+"chef_environment:#{delivery_environment}"
+nodes = delivery_chef_server_search(:node, search_query.to_s)
+
+nodes.each do |i_node|
+  delivery_inspec "inspec_#{node['delivery']['change']['project']}" do
+    infra_node i_node['ipaddress']
+    os i_node['os']
+    inspec_test_path 'test/smoke'
+  end
+end
